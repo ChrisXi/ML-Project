6dd08f328e648bd13f20bab50ee25d4204806132
Chen Wang <leonw007@gmail.com>
2015-12-07 14:22:40 -0500

    priceton matrix libary

--- src/cs475/NeuralNetwork.java
+++ src/cs475/NeuralNetwork.java
@@ -2,6 +2,7 @@ package cs475;
 
 import java.util.ArrayList;
 import java.util.List;
+import PrincetonMatrix.Matrix;
 
 public class NeuralNetwork extends Predictor{
 
@@ -19,8 +20,9 @@ public class NeuralNetwork extends Predictor{
 		for(int n=0; n<this.neuronNum.length-1; n++) {
 			int preNeuronNum = neuronNum[n]+1; //neuron Number (In previous neuron Layer) plus one bias
 			int postNeuronNum = neuronNum[n+1]; //neuron Number In next neuron Layer
-			double weights[][] = new double[preNeuronNum][postNeuronNum]; 
+//			double weights[][] = new double[preNeuronNum][postNeuronNum]; 
 			
+			double[][] weights = Matrix.random(preNeuronNum, postNeuronNum);
 			this.totalWeights.add(weights);
 		}
 	}
