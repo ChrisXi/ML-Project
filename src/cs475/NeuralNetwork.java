0c1377ccec93da45bee55bea823f20f972daac85
Xi Yang <clcblueljw@gmail.com>
2015-12-07 15:06:48 -0500

    add activiton

--- src/cs475/NeuralNetwork.java
+++ src/cs475/NeuralNetwork.java
@@ -12,22 +12,32 @@ public class NeuralNetwork extends Predictor{
 
 	private List<double[][]> totalWeights;
 	int neuronNum[]; //neuron Number In Each Layers
+	List<double[]> totalActValues; //activation in each layers
 	
 	public NeuralNetwork() {
-		this.totalWeights = new ArrayList<double[][]>();
-		
+		this.totalWeights = new ArrayList<double[][]>();	
 		for(int n=0; n<this.neuronNum.length-1; n++) {
-			int preNeuronNum = neuronNum[n]+1; //neuron Number (In previous neuron Layer) plus one bias
-			int postNeuronNum = neuronNum[n+1]; //neuron Number In next neuron Layer
+			int preNeuronNum = this.neuronNum[n]+1; //neuron Number (In previous neuron Layer) plus one bias
+			int postNeuronNum = this.neuronNum[n+1]; //neuron Number In next neuron Layer
 			double weights[][] = new double[preNeuronNum][postNeuronNum]; 
 			
 			this.totalWeights.add(weights);
+		}
+		
+		this.totalActValues = new ArrayList<double[]>();
+		for(int n=0; n<this.neuronNum.length-1; n++) {
+			int neuronNum = this.neuronNum[n]+1; //neuron Number (In previous neuron Layer) plus one bias
+			double[] actValues = new double[neuronNum];
+			actValues[0] = 1; //bias equals to 1
+			this.totalActValues.add(actValues);
 		}
 	}
 	
 	@Override
 	public void train(List<Instance> instances) {
 		// TODO Auto-generated method stub
+		
+		
 		
 	}
 
