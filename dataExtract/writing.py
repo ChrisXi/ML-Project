a9e0d07ba0a23b0e466cfde912dfa0d13798a163
Chen Wang <leonw007@gmail.com>
2015-12-08 22:23:50 -0500

    modifiy extract python file for smaller size data

--- dataExtract/writing.py
+++ dataExtract/writing.py
@@ -53,9 +53,12 @@ for item in t.get('data'):
 training_data = [(x, y) for x,y in zip(dataR,  a.get("labels"))]
 
 test_data = [(x, y) for x,y in zip(dataRt,  t.get("labels"))]
-f = open("test.txt","w")
+f = open("subtest.txt","w")
 
-for item in test_data:
+subtrain_data = training_data[0:500]
+subtest_data = test_data[0:500]
+
+for item in subtest_data:
 	f.write(str(item[1]))
 	for i in range(len(item[0])):
 	   f.write(" "+str(i+1)+":"+str(item[0][i][0]))
